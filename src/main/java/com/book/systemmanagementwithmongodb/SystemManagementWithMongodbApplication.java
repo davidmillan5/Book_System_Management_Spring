a0ceaa89ffb1250ba0d753c0e53d27b9300c29ec
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-06-26 19:32:59 -0500

    Initial Commit

---
+++ src/main/java/com/book/systemmanagementwithmongodb/SystemManagementWithMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.book.systemmanagementwithmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemManagementWithMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemManagementWithMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/book/systemmanagementwithmongodb/SystemManagementWithMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.systemmanagementwithmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemManagementWithMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
